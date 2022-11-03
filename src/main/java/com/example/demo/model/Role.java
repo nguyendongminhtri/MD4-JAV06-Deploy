e34f80274fca5eb31da1da075b190d1e5ef7f9cc
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2022-11-03 22:09:53 +0700

    add lombok

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -1,11 +1,17 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,27 +21,27 @@ public class Role {
     @Column(length = 60)
     private RoleName name;
 
-    public Role() {
-    }
-
-    public Role(Long id, RoleName name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public RoleName getName() {
-        return name;
-    }
-
-    public void setName(RoleName name) {
-        this.name = name;
-    }
+//    public Role() {
+//    }
+//
+//    public Role(Long id, RoleName name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public RoleName getName() {
+//        return name;
+//    }
+//
+//    public void setName(RoleName name) {
+//        this.name = name;
+//    }
 }
